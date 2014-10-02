9b1862399aa84126b8b05282c69a07208f03d627
computer <computer@localhost.localdomain>
2014-10-02 12:46:16 -0300

    first commit

---
+++ ControlMouseKeyboard/src/CaptureKeyboard.java
@@ -0,0 +1,77 @@
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.Panel;
+import java.awt.event.KeyEvent;
+
+import javax.swing.InputMap;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+
+public class CaptureKeyboard extends JFrame{
+//JComponent.WHEN_IN_FOCUSED_WINDOW
+	
+/*	Here is an example of specifying that a component should react to the F2 key:
+
+		component.getInputMap().put(KeyStroke.getKeyStroke("F2"), "doSomething");
+		component.getActionMap().put("doSomething", anAction);
+		
+		component.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "pressed");
+		component.getActionMap().put("pressed", pressedAction);
+
+		component.getInputMap().put(KeyStroke.getKeyStroke("released SPACE"), "released");
+        component.getActionMap().put("released", releasedAction);
+		
+		
+		//where pressedAction and releasedAction are javax.swing.Action objects
+		//where anAction is a javax.swing.Action*/
+	JButton b1;
+	
+	public CaptureKeyboard(){
+		Panel p = new Panel();
+		p.setSize(300, 300);
+		
+		setSize(300, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		b1 = new JButton("abc");
+		b1.setSize(10, 10);;
+		b1.setVisible(true);
+		
+		p.add(b1);
+		
+		add(p);
+		setVisible(true);
+
+
+		
+	}
+	/*
+	public static void main(String args[]){
+		new CaptureKeyboard();
+	}*/
+	
+	/*protected void addBindings() {
+        InputMap inputMap = textPane.getInputMap();
+ 
+        //Ctrl-b to go backward one character
+        KeyStroke key = KeyStroke.getKeyStroke(KeyEvent.VK_B, Event.CTRL_MASK);
+        inputMap.put(key, DefaultEditorKit.backwardAction);
+ 
+        //Ctrl-f to go forward one character
+        key = KeyStroke.getKeyStroke(KeyEvent.VK_F, Event.CTRL_MASK);
+        inputMap.put(key, DefaultEditorKit.forwardAction);
+ 
+        //Ctrl-p to go up one line
+        key = KeyStroke.getKeyStroke(KeyEvent.VK_P, Event.CTRL_MASK);
+        inputMap.put(key, DefaultEditorKit.upAction);
+ 
+        //Ctrl-n to go down one line
+        key = KeyStroke.getKeyStroke(KeyEvent.VK_N, Event.CTRL_MASK);
+        inputMap.put(key, DefaultEditorKit.downAction);
+    }*/
+	
+	
+}
---
+++ ControlMouseKeyboard/src/KeyPress.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.awt.event.*;
+/** This code was based on the example of this site <http://www.roseindia.net/java/example/java/awt/KeyPress.shtml>,
+ * that demonstrate how can I capture a key of keyboard and firing events on a frame.
+ * 
+ * So I used this approach for moving a mouse cursor through the frame using the Robot class.
+ */
+
+public class KeyPress extends Frame {
+	
+	Robot r;
+	int x = 683;
+	int y = 384;
+
+	public static void main(String[] args) throws AWTException {
+		KeyPress k = new KeyPress();
+	}
+
+	public KeyPress() throws AWTException {
+		
+		super("Key Press Event Frame");
+		r = new Robot();
+		
+		Panel panel = new Panel();
+		panel.setSize(1366, 768);
+		panel.addKeyListener(new MyKeyListener());
+		panel.setVisible(true);
+		
+		add(panel);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+		setSize(1366, 768);
+		setVisible(true);
+		r.mouseMove(x, y);
+	}
+
+	public class MyKeyListener extends KeyAdapter {
+		
+		public void keyPressed(KeyEvent ke) {
+			
+			char i = ke.getKeyChar();
+			
+			if(i == 'w' && y > -1)
+				r.mouseMove(x, y-=3);
+			else if(i == 's' && y < 769)
+				r.mouseMove(x, y+=3);
+			
+			//TODO the others directions
+			
+		}
+		
+	}
+	
+}
